94cbce55ab703c14f1b77a325a35caec60c3ad03
Mars Wong <marswong618@gmail.com>
2018-02-01 03:27:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,14 +14,14 @@ class Triangle
 
     if a <= 0 || b <= 0 || c <= 0
       raise TriangleError
+    elsif a + b < c || a + c < b || b + c < a || a - b > c || a - c > b || b - c > a
+      raise TriangleError
     elsif a == b && b == c
       :equilateral
     elsif a == b || b == c || a == c
       :isosceles
-    elsif ((a + b > c && a - b < c) || (a + c > b && a - c < b) || (b + c > a || b - c < a))
-      :scalene
     else
-      raise TriangleError
+      :scalene
     end
   end
 end
