772d188c1b8280a1c56536cb0307f6c0fdc2dd01
Mars Wong <marswong618@gmail.com>
2018-02-01 03:28:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
 
     if a <= 0 || b <= 0 || c <= 0
       raise TriangleError
-    elsif a + b < c || a + c < b || b + c < a || a - b > c || a - c > b || b - c > a
+    elsif a + b <= c || a + c <= b || b + c <= a || a - b >= c || a - c >= b || b - c >= a
       raise TriangleError
     elsif a == b && b == c
       :equilateral
