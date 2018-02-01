67c63f3c8e156c42936c81410758af71f0e27321
Mars Wong <marswong618@gmail.com>
2018-02-01 03:17:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,7 +4,7 @@ class Triangle
   def initialize(a, b, c)
     if a == b && b == c
       @kind = :equilateral
-    elsif a == b || b == c
+    elsif a == b || b == c || a == c
       @kind = :isosceles
     elsif a > 0 && b > 0 && c > 0 &&
           ((a + b > c && a - b < c) || (a + c > b && a - c < b) || (b + c > a || b - c < a))
