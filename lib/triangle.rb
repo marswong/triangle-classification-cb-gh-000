692e804b829e8ebdb96166316133072124852b12
Mars Wong <marswong618@gmail.com>
2018-02-01 03:13:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,8 @@ class Triangle
       @kind = :equilateral
     elsif a == b || b == c
       @kind = :isosceles
-    elsif a > 0 && b > 0 && c > 0 && ((a + b > c && a - b < c) || (a + c > b && a - c < b) || (b + c > a || b - c < a))
+    elsif a > 0 && b > 0 && c > 0 &&
+          ((a + b > c && a - b < c) || (a + c > b && a - c < b) || (b + c > a || b - c < a))
             
     end
   end
